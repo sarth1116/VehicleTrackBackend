851ed09c1ea0c24a6bf38f01e4b3f37a1a7123bf
sarthak <sarthaklahare1234@gmail.com>
2024-10-25 21:50:11 +0530

    Backend

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 4000;
+const vehicleData = require('./data.json')
+// Use CORS middleware
+app.use(cors()); // Enable CORS for all routes
+
+// Fetch vehicle data based on the date
+app.get('/api/route/:date', (req, res) => {
+    const date = req.params.date;
+    if (vehicleData[date]) {
+        res.json(vehicleData[date]);
+    } else {
+        res.status(404).send("No data found for this date");
+    }
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
