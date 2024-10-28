917f614e419d6213b857abc97a919a4bf72eeb6c
sarthak <sarthaklahare1234@gmail.com>
2024-10-28 20:05:41 +0530

    change

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
+const fs = require('fs');
+const path = require('path');
+const app = express(); // This line initializes your Express app
 const port = 4000;
-const vehicleData = require('./data.json')
+
 // Use CORS middleware
-app.use(cors({origin: "https://sarth1116.github.io"})); // Enable CORS for all routes
+app.use(cors()); // Enable CORS for all routes
 
 // Fetch vehicle data based on the date
 app.get('/api/route/:date', (req, res) => {
     const date = req.params.date;
-    if (vehicleData[date]) {
-        res.json(vehicleData[date]);
-    } else {
-        res.status(404).send("No data found for this date");
-    }
+    const dataPath = path.join(__dirname, 'data.json');
+    
+    // Read and parse data.json dynamically
+    fs.readFile(dataPath, 'utf-8', (err, data) => {
+        if (err) {
+            console.error("Error reading data.json:", err);
+            res.status(500).send("Internal server error");
+            return;
+        }
+        
+        const vehicleData = JSON.parse(data);
+        
+        if (vehicleData[date]) {
+            res.json(vehicleData[date]);
+        } else {
+            res.status(404).send("No data found for this date");
+        }
+    });
 });
 
 // Start the server
